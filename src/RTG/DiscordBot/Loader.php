41ab96ed6c4567ce28ccdf9c141f0b8c5a49f47d
RTG <igadget28@gmail.com>
2017-03-27 17:16:07 +0800

    Tested | Left with Task :)

--- src/RTG/DiscordBot/Loader.php
+++ src/RTG/DiscordBot/Loader.php
@@ -22,35 +22,83 @@ namespace RTG\DiscordBot;
 /* Essentials */
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\event\Listener;
 
-use RTG\DiscordBot\Task;
+use RTG\DiscordBot\TaskMCPE;
 
-class Loader extends PluginBase {
+class Loader extends PluginBase implements Listener {
     
     public $cfg;
     
     public function onEnable() {
         
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+            
         @mkdir($this->getDataFolder());
         $this->cfg = new Config($this->getDataFolder() . 'config.yml', Config::YAML, array(
             "webhook" => "",
-            "username" => "Bot",
+            "username" => "",
+            "serverhost" => "",
             "# Shutdown",
             "enable" => false,
-            "ticks" => 500,
+            "ticks" => 1,
             "shut_message" => ""
         ));
-        $this->cfg->save();
         
         $period = $this->cfg->get("ticks");
         
-        $this->getServer()->getScheduler()->scheduleTask(new Task($this), $period);
+        $this->getServer()->getScheduler()->scheduleTask(new TaskMCPE($this), $period);
+        
+        if($this->cfg->get("serverhost") === "") {
+            $this->getLogger()->warning("Make sure you have set your ServerHost before using this plugin!");
+            $this->setEnabled(false);
+        }
+        
+    }
+    
+    public function run() {
+        
+        if($this->cfg->get("enable") === true) {
+                
+            $this->getLogger()->warning("hey");
+                
+            $command = "say hi"; // used for testing!
+                
+            $msg = $this->cfg->get("shut_message");
+            $user = $this->cfg->get("username");
+            $url = $this->cfg->get("webhook");
+            $host = $this->cfg->get("serverhost");
+
+            /* Thanks Niekert for these lines! */
+                
+            $curl  = curl_init();
+            $line = array("content" => $msg, "username" => "$user");
+                
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($line));
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+                    
+            curl_exec($curl);
+                    
+            /* ------ */
+            
+            if($host === strtolower("multicraft")) {
+                $this->getLogger()->warning("Good bye!");
+                $this->getServer()->dispatchCommand(new \pocketmine\command\ConsoleCommandSender, $command);
+            }
+            else {
+                $this->getLogger()->warning("You can't use this function on other servers yet!");
+                $this->setEnabled(false);
+            }
+        
+        }
         
     }
     
     public function onDisable() {
-        
-        $this->cfg->save();
         
     }
     
